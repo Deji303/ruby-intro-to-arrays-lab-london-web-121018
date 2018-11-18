b09885bb9b9de8e2cb20515e78271f8c90d6b042
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-11-18 16:28:44 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,11 +2,14 @@ def instantiate_new_array
   my_new_array = [];
 end 
 
+def array_with_two_elements
+  my_two_array = [1,2];
+end 
 
 
 
 
-my_two_array = [1,2];
+
 
 def my_first_element (array) 
   array[0]
